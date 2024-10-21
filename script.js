33ec0473d0a5a570f46f5f87bf6d2c9ab2bada54
Tsukii-stitch <christianklyde14@gmail.com>
2024-10-21 19:12:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//Global Variable
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
+
+//Hamburger Menu Function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+    if (menu.style.display === "block" && logo.style.display === "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
